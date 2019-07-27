d8d746bb37453ecb5a760d82fc7243e92c5730f2
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-07-27 11:00:29 +0300

    Use `react-bootstrap`
    - Install `react-bootstrap`
    - Import `bootstrap.css`

--- on-this-day-frontend/src/index.js
+++ on-this-day-frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import Article from './articles/Article';
 
 ReactDOM.render(<Article />, document.getElementById('root'))
