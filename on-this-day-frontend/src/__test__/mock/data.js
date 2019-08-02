402e703d1e8a99ad91fd641701d2285723e73efb
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-08-02 09:33:32 +0300

    Adds mock data for testing the `Article` component

---
+++ on-this-day-frontend/src/__test__/mock/data.js
@@ -0,0 +1,25 @@
+const articles = [
+	{
+		title: "Dolorem sint exercitationem fugit sed necessitatibus.",
+		titleImageSrc: "http://lorempixel.com/640/480/cats",
+		titleImageCaption: "Quos quae laudantium id ab non alias esse.",
+		subtitle: "Iste sed rerum ut nisi facilis aut veritatis.",
+		content: [
+			"Ipsa voluptatem aperiam numquam deserunt labore aut. Vitae ullam cupiditate incidunt nobis eaque quas ut. Blanditiis et quidem deserunt molestias corporis dignissimos sint.",
+			"Voluptas possimus ea et dignissimos dolorem accusamus numquam nihil. Maiores in quisquam dolorem. Sed perspiciatis aperiam aut. Est quia tenetur quisquam vero. Tempora et fuga consequatur in neque officiis totam rem.",
+			"Sed dolorem incidunt totam. Maxime enim vel. Odio distinctio sed tenetur iure doloribus harum. Debitis aut alias ea error veritatis. Cupiditate fuga tenetur eos."
+		],
+		author: "Monty Gaylord",
+		date: "Thu Aug 01 2019",
+		rating: 1,
+		comments: [
+			"Adipisci omnis in voluptatem vel fugit cum autem.",
+			"Odio perspiciatis quibusdam veritatis nam quibusdam et aliquid.",
+			"Autem aliquam quidem quo et fugit est dolor."
+		]
+	}
+]
+
+export {
+	articles
+}
