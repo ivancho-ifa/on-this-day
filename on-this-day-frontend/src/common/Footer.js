0841e0132057e9998eb8837583b8f2c9d0cd628b
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-08-02 09:30:54 +0300

    Adds `Footer` component

---
+++ on-this-day-frontend/src/common/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+/**
+ * @todo Make it stick to the bottom regardless the rest content height.
+ */
+
+function Footer() {
+	return <footer className="footer p-3 text-center">
+		<span className="text-muted">Follow "On This Day" on <a href="https://www.facebook.com/natoziden681/">Facebook</a></span>
+	</footer>
+}
+
+export default Footer
