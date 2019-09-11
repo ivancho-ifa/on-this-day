6ef88a2d41971dab82a4b753b67509f0ba0b66ec
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-09-11 20:34:12 +0300

    Implements '/users/user-:id/edit'

--- on-this-day-backend/routes/users.js
+++ on-this-day-backend/routes/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const ObjectID = require('mongodb').ObjectID
 
+const authz = require('./utils/authz')
 const errors = require('../utils/errors')
 
 
@@ -22,6 +23,24 @@ router.get('/users/user-:id', (request, response, next) => {
 		if (!user) return next(new errors.RequestHandlingError(404, `User with ID ${request.params.id} not found!`))
 
 		return response.send(user)
+	})
+})
+
+router.post('/users/user-:id/edit', authz, (request, response, next) => {
+	const db = request.app.locals.db
+
+	if (request.authnData.userID !== request.params.id)
+		return next(new errors.RequestHandlingError(401))
+
+	db.collection('users').updateOne({
+		_id: new ObjectID(request.params.id)
+	}, {
+		$set: request.body
+	}, (error, result) => {
+		if (error) return next(error)
+		if (result.matchedCount !== 1) return next(new errors.ArticleNotFound(request.params.id))
+
+		return response.status(204).end()
 	})
 })
 
